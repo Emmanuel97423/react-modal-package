04c0d7436702e3a10a0b76ef4fc690f7c8cb767a
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-06-05 11:55:16 +0400

    Fix(modal): delete useState React hook in Modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
  import Modal from './components/Modal';
 
 
 
 const App: React.FC = () => {
+     const [openModal, setOpenModal] = useState(false);
+
+    const handleModal:() => void = ()=>{
+        setOpenModal(true)
+        }
+
     return(
         <div>
-            <Modal width={''} height={''} content={undefined} open={false}/>
+            <Modal width={'200'} height={'300'} open={openModal} content="Example" handleModal={handleModal}/>
         </div>
     )
 }
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,31 +1,27 @@
 import React from 'react';
-import {ReactNode, useState} from 'react';
+import {ReactNode} from 'react';
 
 interface ModalProp {
     width: string;
     height: string;
     content:ReactNode;
     open:boolean;
+    handleModal:(e:React.MouseEvent<HTMLButtonElement>, open:boolean) => void;
 }
 
-const Modal:React.FC<ModalProp> = ({width, height, content,open})=>{
-    const [openModal, setOpenModal] = useState(open);
-
-    const handleModal:() => void = ()=>{
-        setOpenModal(true)
-        }
-
+const Modal:React.FC<ModalProp> = ({width, height, content,open, handleModal})=>{
+   
 
     return(
         <>
        
-        <div className={`${openModal ? 'fixed  inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' : 'hidden' }`}>
+        <div className={`w-2 ${open ? 'fixed  inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' : 'hidden' }`}>
             <div className={`w-[${width}px] h-[${height}px]  flex flex-col`}>
-                <button className="text-white text-xl place-self-end" onClick={()=>{setOpenModal(false)}}>x</button>
+                <button className="text-white text-xl place-self-end" onClick={(e)=>{handleModal(e,false)}}>x</button>
                 <div className="bg-white p-4 rounded text-black">{content}</div>
             </div>
         </div>
-        <button onClick={handleModal}>Active Modal</button>
+        <button onClick={(e)=>handleModal(e,true)}>Active Modal</button>
         </>
     )
 }
